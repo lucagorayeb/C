6c1b468604c7552d9620a268e1c8465527760ba3
Luca Siqueira Assis Gorayeb de Mello <lucagorayeb@gmail.com>
2026-03-11 08:48:52 -0400

    -

--- estudos/cap_5_funcoes/exercicios/guess_number.c
+++ estudos/cap_5_funcoes/exercicios/guess_number.c
@@ -10,16 +10,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+int randomNumber(void);
+void isCorrect(int, int);
+void wrongAnswer(int);
+void numberGuessed(void);
+void playGameAgain(void);
+int askIfWantToPlayAgain(void);
+void numberToHigh(void);
+void numberToLow(void);
+
 int main() {
     srand(time(NULL));
     
-    int number;
+    int number, random_number = randomNumber();
 
     printf("I have a number betwen 1 and 1000\n");
     printf("Can you guess the number?\n");
     printf("Please, type your first try: ");
     scanf("%d", &number);
-    
+    isCorrect(number, random_number);
     return 0;
 }
 
@@ -27,13 +36,51 @@ int randomNumber(){
     return 1 + rand() % 1000;
 }
 
-char isCorrect(int number, int random_number){
+void isCorrect(int number, int random_number){
     if(number > random_number){
-        return "To higher. Try again.";
+        numberToHigh();
+        wrongAnswer(random_number);
     }else if(number < random_number){
-        return "To low, Try again.";
-    }else{
-        return "Excelent! You guess the number! Would you like to try again?"
+        numberToLow();
+        wrongAnswer(random_number);
+    }else if(number == random_number){
+        numberGuessed();
     }
 }
+
+void wrongAnswer(int random_number){
+    int number;
+    printf("Type your guess: ");
+    scanf("%d", &number);
+    isCorrect(number, random_number);
+}
+
+void numberGuessed(){
+        printf("Excelent! You guess the number!\n");
+        playGameAgain();
+}
+
+void playGameAgain(){
+    if(askIfWantToPlayAgain() == 1){
+        main();
+    }else{
+        printf("Thanks for playing!\n");
+    }
+}
+
+int askIfWantToPlayAgain(){
+        int answer;
+        printf("Would you like to try again? [S-1/N-2] ");
+        scanf("%d", &answer);
+        return answer;
+}
+
+void numberToHigh(){
+        printf("To higher. Try again.\n");
+}
+
+void numberToLow(){
+        printf("To low, Try again.\n");
+}
+
 
